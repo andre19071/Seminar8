05d2c69f14062cdb9b8587e8e206f44427360e18
Наталья <109668015+andre19071@users.noreply.github.com>
2022-09-01 19:27:12 +0700

    task57

--- task57/Program.cs
+++ task57/Program.cs
@@ -9,3 +9,51 @@
 // 4 встречается 1 раз
 // 6 встречается 2 раза
 // В нашей исходной матрице встречаются элементы от 0 до 9
+int[,] FillMatrix(int rowsCount, int columnsCount, int leftRange = 0, int rightRange = 9)
+{
+    int[,] matrix = new int[rowsCount, columnsCount];
+    Random rand = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[] array = new int[10];
+Console.WriteLine("Введите число строк");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число столбцов");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = FillMatrix(m, n);
+PrintMatrix(matrix);
+Console.WriteLine();
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        array[matrix[i, j]] = array[matrix[i, j]] + 1;
+    }
+}
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        Console.WriteLine($"Количество {i} = {array[i]}");
+    }
+}
